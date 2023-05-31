a57f6690d58b7bd1a5ec7e6929c19ed8a2fd7824
arvifox <arvifox@hotmail.com>
2023-05-31 22:21:37 +0300

    new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,27 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World! 681")
 
-    val inputFile = File(args.first())
+    val inputFile = File(args[1])
     val inputText = inputFile.readText()
 
-    File(args[1]).walkTopDown().forEach {
+    var count = 0
+    File(args[0]).walkTopDown().forEach {
         if (it.isFile && it.extension == "kt") {
-            val content = it.readText()
-            if (!content.startsWith(inputText)) {
+            val lines = it.readLines()
+            if (lines[1].contains("pekrin", true)) {
+                val without = lines.subList(4, lines.lastIndex + 1).joinToString("\n")
                 it.writeText(inputText)
+                it.appendText(without)
                 it.appendText("\n")
-                it.appendText(content)
+                count++
             }
         }
     }
+    println("count = $count")
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Done.")
+    println("Done")
 }
